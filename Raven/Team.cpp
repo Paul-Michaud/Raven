9825b549bf41e9bb3ff85b89a9a8b515873d8f32
renald-morice <renaldmorice@hotmail.fr>
2017-11-30 10:28:01 -0500

    add Team class (not complete yet)

---
+++ Raven/Team.cpp
@@ -0,0 +1,31 @@
+#include "Team.h"
+
+
+//-----------------------
+//     Constructor
+//-----------------------
+Team::Team(){
+}
+
+//-----------------------
+// Add member to the team
+//-----------------------
+void Team::addMember(Raven_Bot &)
+{
+}
+
+//-----------------------
+// Assign a new leader for the team
+//-----------------------
+void Team::setLeader(Raven_Bot &)
+{
+}
+
+//-----------------------
+// Assign a new leader for the team
+//-----------------------
+void Team::setTarget(Raven_Bot &)
+{
+}
+
+
